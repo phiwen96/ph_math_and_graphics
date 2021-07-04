643a4591bd5633cdd57f4fba24308ffee320ca39
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-04 16:00:34 +0200

    a

--- include/ph_math_and_graphics/math_and_graphics.hpp
+++ include/ph_math_and_graphics/math_and_graphics.hpp
@@ -1,4 +1,41 @@
 #pragma once
 
+//template <unsigned long long>
+//struct point
+//{
+//    unsigned long long d;
+//
+//    consteval point (unsigned long long d) : d {d}
+//    {
+//
+//    }
+//
+//    consteval auto operator () (auto... coords)
+////    requires (sizeof... (coords) == d)
+//    {
+//        d = sizeof... (coords);
+//        return *this;
+//    }
+//
+//    friend std::ostream& operator<< (std::ostream& os, point const& p)
+//    {
+//        return os << "point <" << p.d << ">";
+//    }
+//};
 
+inline auto point (auto&&... coords) 
+{
+    
+}
 
+//template <typename T, T A>
+//consteval point operator "" d (unsigned long long a) //-> point<a>
+//{
+//    return {a};
+//}
+
+//point operator "" _p (point)
+//{
+//    return {};
+//}
+//auto point ()
--- tests/test_math_and_graphics.cpp
+++ tests/test_math_and_graphics.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch.hpp>
-
+#include <ph_math_and_graphics/math_and_graphics.hpp>
+using namespace std::literals::string_literals;
 //
 //
 //
@@ -14,6 +15,26 @@ TEST_CASE ("Constructs and assignments")
 //
 //		}
 //	}
+    
 //}
-    std::cout << "hi" << std::endl;
+    
+    for (auto const& str : {"foo"s, "bar"s, "baz"s})
+      {
+//        process(str);
+      }
+    
+//    for (auto s : {3d (3, 4, 5), 4d})
+//    {
+//
+//    }
+    
+//    std::cout << 33d << std::endl;
+//    std::cout << "hi" << std::endl;
+//    std::cout << 2d (3, 2) << std::endl;
+//
+//    auto a = 3d;
+//    std::cout << a << std::endl;
+    
+    
+//    auto p = point (3, 4);
 }
