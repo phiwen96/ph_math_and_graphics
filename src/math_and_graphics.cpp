1555ae9f2cb8dfd3b6457b8a54bfbe0dfb14a3b3
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-04 15:23:09 +0200

    a

---
+++ src/math_and_graphics.cpp
@@ -0,0 +1,1 @@
+#include "math_and_graphics.hpp"
---
+++ tests/test_math_and_graphics.cpp
@@ -0,0 +1,19 @@
+#include <catch2/catch.hpp>
+
+//
+//
+//
+TEST_CASE ("Constructs and assignments")
+{
+//GIVEN ("User defined classes A B C D")
+//{
+//	AND_WHEN ()
+//	{
+//		THEN
+//		{
+//
+//		}
+//	}
+//}
+    std::cout << "hi" << std::endl;
+}
